5c735654c2e4389410c14514ee43d38631c275ce
Erin Cargin <ecargin@comcast.net>
2023-07-22 09:26:52 -0700

    Created Find Food Page

---
+++ Freefood/FindFoodView.xaml.cs
@@ -0,0 +1,31 @@
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.UI;
+
+namespace Freefood;
+
+public partial class FindFoodPage : ContentPage
+{
+    private SystemLocationDataSource locationSource = new SystemLocationDataSource();
+    public FindFoodPage()
+	{
+		InitializeComponent();
+        this.BindingContext = new FindFoodViewModel();
+        StartLocationServices();
+	}
+
+    private async Task StartLocationServices()
+    {
+        var status = await Microsoft.Maui.ApplicationModel.Permissions.RequestAsync<Microsoft.Maui.ApplicationModel.Permissions.LocationWhenInUse>();
+
+        mapView.LocationDisplay.DataSource = locationSource;
+        if (status == Microsoft.Maui.ApplicationModel.PermissionStatus.Granted)
+        {
+            await mapView.LocationDisplay.DataSource.StartAsync();
+            mapView.LocationDisplay.IsEnabled = true;
+        }
+
+        mapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Recenter;
+
+    }
+
+}
---
+++ Freefood/FindFoodViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
+using Map = Esri.ArcGISRuntime.Mapping.Map;
+
+namespace Freefood
+{
+    internal class FindFoodViewModel : INotifyPropertyChanged
+    {
+        public static Uri foodUri = new Uri("https://services8.arcgis.com/LLNIdHmmdjO2qQ5q/arcgis/rest/services/FreeFood/FeatureServer/0");
+        private FeatureLayer foodFeatureLayer;
+
+        public FindFoodViewModel()
+        {
+            _map = new Map(SpatialReferences.WebMercator)
+            {
+                InitialViewpoint = new Viewpoint(new Envelope(-180, -85, 180, 85, SpatialReferences.Wgs84)),
+                Basemap = new Basemap(BasemapStyle.ArcGISStreets)
+            };
+
+            foodFeatureLayer = new FeatureLayer(foodUri);
+            _map.OperationalLayers.Add(foodFeatureLayer);
+
+        }
+
+        private Esri.ArcGISRuntime.Mapping.Map _map;
+
+        /// <summary>
+        /// Gets or sets the map
+        /// </summary>
+        public Esri.ArcGISRuntime.Mapping.Map Map
+        {
+            get => _map;
+            set { _map = value; OnPropertyChanged(); }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
